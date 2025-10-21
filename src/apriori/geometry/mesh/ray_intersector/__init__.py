4a336cb6e19e3d581c922dda6a8bc1b410019524
Sergei Shumilin <shumilinss@gmail.com>
2025-10-21 15:00:41 +0000

    add detect_points_inside to  ray_intersection module interface

--- src/apriori/geometry/mesh/ray_intersector/__init__.py
+++ src/apriori/geometry/mesh/ray_intersector/__init__.py
@@ -1,9 +1,11 @@
 from apriori.geometry.mesh.ray_intersector.method import (
     RayTriangleIntersector,
     RayTriangleIntersectorResult,
+    detect_points_inside,
 )
 
 __all__ = [
     "RayTriangleIntersector",
     "RayTriangleIntersectorResult",
+    "detect_points_inside",
 ]
